5e798e7cd9fcb52ed89c96d19368a983b8c1b5f0
AdnanB0 <143035785+AdnanB0@users.noreply.github.com>
2023-09-01 11:13:05 -0400

    Assignment

--- ch00/lab/main.py
+++ ch00/lab/main.py
@@ -21,7 +21,7 @@ pygame.time.wait(500)
 screen.fill([0, 0, 255])
 
 font = pygame.font.SysFont(None, 72)
-text = font.render("Hello Adnan", True, "pink")
+text = font.render("Hello Adnan", True, "green")
 
 screen.blit(text, screen.get_rect().center)
 
