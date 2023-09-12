76ff906ae6b9f29d245d26d812d03367445c91db
AdnanB0 <143035785+AdnanB0@users.noreply.github.com>
2023-09-11 22:30:53 -0400

    EXERCISE CURRENCY CONVERSION

---
+++ ch01/exercises/currency.py
@@ -0,0 +1,7 @@
+rate = input("exchange rate for Euro to Dollar?")
+amount = input("How much would you like to exchange?")
+var1 = int(rate)
+var2 = int(amount)
+total = var1 * var2
+result = total - 3
+print(result)
