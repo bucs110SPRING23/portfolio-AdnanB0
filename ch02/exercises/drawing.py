08e9e4624ccc69ea0076c00e24f7076155edb926
AdnanB0 <143035785+AdnanB0@users.noreply.github.com>
2023-09-18 22:11:04 -0400

    forgot to push

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -2,11 +2,10 @@ import turtle
 
 num_sides = int(input("Enter the number of sides"))
 length = int(input("Enter length of each side"))
-
-turtle.Turtle()
-
+T1 = turtle.Turtle()
 turtle.color("blue")
-
-angle = 360/num_sides 
+for i in range(num_sides):
+    T1.forward(length)
+    T1.right(360/num_sides)
 
 turtle.exitonclick()
