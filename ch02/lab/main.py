10498eb223d6893478dd6aefed2c43ecf9eaf2a9
AdnanB0 <143035785+AdnanB0@users.noreply.github.com>
2023-09-22 12:42:29 -0400

    Chap 2 lab

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,5 +1,7 @@
 import turtle  # 1. import modules
 import random
+import pygame
+import math
 
 # Part A
 window = turtle.Screen()  # 2.  Create a screen
@@ -25,13 +27,13 @@ leonardo.goto(-100, -20)
 #################################################
 
 ## Race 2
-x1 = random.randrange(1,101)
-x2 = random.randrange(1,101)
 michelangelo.up() 
 leonardo.up()
 michelangelo.goto(-100, 20)
 leonardo.goto(-100, -20)
-for _ in range(x1):
+for _ in range(10):
+    x1 = random.randrange(1,10)
+    x2 = random.randrange(1,10)
     michelangelo.forward(x1)
     leonardo.forward(x2)
 michelangelo.goto(-100, 20)
@@ -42,9 +44,35 @@ window.exitonclick()
 ##################################################
 
 ##PART B
-import pygame
-import math
 pygame.init()
-window = pygame.display.set_mode()
+while True:
+    for event in pygame.event.get():
+        pass
+    window = pygame.display.set_mode()
+    points = []
+    num_sides = [3,4,6,20,100,360]
+    side_length = 100
+    xpos = 300
+    ypos = 400
+
+    for sides in num_sides:
+        for i in range(sides):
+            angle = 360/sides
+            radians = math.radians(angle * i)
+            x = xpos + side_length * math.cos(radians)
+            y = ypos + side_length  * math.sin(radians)
+            points.append([x,y])
+        window.fill('white')
+        pygame.draw.polygon(window, 'Blue', points)
+        pygame.display.flip()
+        pygame.time.wait(1000)
+        points = []
+    break
+
+
+
+  
+
+
 
 
